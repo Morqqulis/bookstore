eb02f1970c1911270440353c1b6b62ea663e4562
Dmitriy <morqqulis@gmail.com>
2024-05-09 08:59:35 +0400

    Added sliders in catalog page.

--- js/global.js
+++ js/global.js
@@ -1,30 +1,16 @@
 'use strict'
-/* Swup Animation */
-import Swup from 'https://unpkg.com/swup@4?module'
-
-const swup = new Swup({
-	animateOnLoad: true,
-	plugins: [
-		new SwupHeadPlugin({
-			persistAssets: true,
-		}),
-	],
-})
-swup.transitions = {
-	default: {
-		animateOnLoad: true,
-	},
-}
 
 /* ======================================================================== */
-
+localStorage.setItem('genre', 'Frontend')
 /* Modal */
 const html = document.documentElement
 
 html.addEventListener('click', e => {
 	const modalTrigger = e.target.closest('.header__action')
 	const modal = document.getElementById('modal')
+	const genreButtons = document.querySelectorAll('.catalog__genres-btn')
 
+	/* Open || Close Modal */
 	if (e.target == modalTrigger) {
 		modal.show()
 		html.classList.add('modal-open')
@@ -32,8 +18,31 @@ html.addEventListener('click', e => {
 		modal.close()
 		html.classList.remove('modal-open')
 	}
+
+	/* Active navigation */
+	if (e.target.closest('.header__menu-link')) {
+		navLinks.forEach(link => link.classList.remove('header__menu-link_active'))
+		e.target.classList.add('header__menu-link_active')
+	}
+
+	/* Set active genre */
+	if (e.target.closest('.catalog__link')) {
+		localStorage.setItem('genre', e.target.textContent)
+	}
+
+	if (e.target.closest('.catalog__genres-btn')) {
+		genreButtons.forEach(button => button.classList.remove('catalog__genres-btn_active'))
+		e.target.classList.add('catalog__genres-btn_active')
+		localStorage.setItem('genre', e.target.textContent)
+
+		appendSliderItems()
+		firstSlider.update()
+		secondSlider.update()
+		thirdSlider.update()
+	}
 })
 
+/* ========================Close Modal by Esc============================= */
 const handleCloseModalByEsc = e => {
 	if (e.key === 'Escape') {
 		const modal = document.getElementById('modal')
@@ -45,15 +54,103 @@ const handleCloseModalByEsc = e => {
 document.addEventListener('keydown', handleCloseModalByEsc)
 
 /* ===================================================================== */
-/* Active navigation */
+/* Sliders */
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+const swiperWrappers = document.querySelectorAll('.swiper-wrapper')
 
-const navLinks = document.querySelectorAll('.header__menu-link')
+const getBooks = async genre => {
+	const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${genre}`)
 
-navLinks.forEach(link => {
-	link.addEventListener('click', e => {
-		navLinks.forEach(link => link.classList.remove('header__menu-link_active'))
-		e.target.classList.add('header__menu-link_active')
+	if (!response.ok) {
+		throw new Error(`Error: ${response.status}`)
+	}
+	const booksData = await response.json()
+
+	return booksData
+}
+
+const appendGenre = genre => {
+	const genreList = document.querySelector('.catalog__genres')
+	genreList.insertAdjacentHTML(
+		'afterbegin',
+		`
+        <li class="catalog__genres-item">
+            <button class="catalog__genres-btn catalog__genres-btn_active" type="button" title="genre">${genre}</button>
+        </li>
+    `
+	)
+}
+
+const appendSliderItems = () => {
+	getBooks(localStorage.getItem('genre')).then(data => {
+		data.items.forEach(item => {
+			if (item.volumeInfo.imageLinks) {
+				swiperWrappers.forEach(swiperWrapper => {
+					swiperWrapper.insertAdjacentHTML(
+						'afterbegin',
+						`
+                            <div class="swiper-slide catalog__slide">
+                                <div class="catalog__card">
+                                    <img class="catalog__card-img" src="${item.volumeInfo.imageLinks.thumbnail}" alt="book" width="135" height="180" loading="lazy">
+                                    <div class="catalog__card-info">
+                                        <span class="catalog__card-name">${item.volumeInfo.title}</span>
+                                        <span class="catalog__card-author">${item.volumeInfo.authors}</span>
+                                        <button class="catalog__card-btn btn" type="button" title="read">READ MORE</button>
+                                    </div>
+                                </div>
+                            </div>
+                        `
+					)
+				})
+			}
+		})
 	})
+
+	swiperWrappers.forEach(wrapper => (wrapper.innerHTML = ''))
+}
+
+if (document.querySelector('.catalog__genres')) {
+	appendGenre(localStorage.getItem('genre'))
+}
+
+if (localStorage.getItem('genre')) {
+	appendSliderItems()
+}
+
+const firstSlider = new Swiper('.catalog__first-slider', {
+	slidesPerView: 5,
+	spaceBetween: 60,
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+})
+
+const secondSlider = new Swiper('.catalog__second-slider', {
+	slidesPerView: 5,
+	spaceBetween: 60,
+	loop: true,
+	Autoplay: {},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+})
+
+const thirdSlider = new Swiper('.catalog__third-slider', {
+	slidesPerView: 5,
+	spaceBetween: 60,
+	loop: true,
+
+	Autoplay: {
+		delay: 3000,
+	},
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 })
 
 /* ===================================================================== */
