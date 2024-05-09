107428066a48ea6758c1ec7b3eeee81cea936825
Dmitriy <morqqulis@gmail.com>
2024-05-09 04:07:44 +0400

    Added animation between pages navigation

--- js/global.js
+++ js/global.js
@@ -1,5 +1,24 @@
 'use strict'
-/* Modals */
+/* Swup Animation */
+import Swup from 'https://unpkg.com/swup@4?module'
+
+const swup = new Swup({
+	animateOnLoad: true,
+	plugins: [
+		new SwupHeadPlugin({
+			persistAssets: true,
+		}),
+	],
+})
+swup.transitions = {
+	default: {
+		animateOnLoad: true,
+	},
+}
+
+/* ======================================================================== */
+
+/* Modal */
 const html = document.documentElement
 
 html.addEventListener('click', e => {
@@ -24,4 +43,17 @@ const handleCloseModalByEsc = e => {
 }
 
 document.addEventListener('keydown', handleCloseModalByEsc)
+
+/* ===================================================================== */
+/* Active navigation */
+
+const navLinks = document.querySelectorAll('.header__menu-link')
+
+navLinks.forEach(link => {
+	link.addEventListener('click', e => {
+		navLinks.forEach(link => link.classList.remove('header__menu-link_active'))
+		e.target.classList.add('header__menu-link_active')
+	})
+})
+
 /* ===================================================================== */
