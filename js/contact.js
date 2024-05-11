e45c1b80f3fc9a5959bcbe839102c6c0a957332e
AlasgarovHaji <hacielesgerov0011@gmail.com>
2024-05-11 23:28:38 +0400

    "I added project setContactsData"

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+import { setDBData } from "./global.js";
+
+const setContactsData = () => {
+    const name = document.querySelector('.fullName').value 
+    const email = document.querySelector('.email').value 
+    const adress = document.querySelector('.adress').value
+    const phone = document.querySelector('.phone').value
+    const note = documetn.querySelector('.note').value
+}
