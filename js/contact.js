0e8395550114918074be7553c6dd10a535146115
AlasgarovHaji <hacielesgerov0011@gmail.com>
2024-05-11 23:33:41 +0400

    "I added user Contact Data"

--- js/contact.js
+++ js/contact.js
@@ -3,7 +3,17 @@ import { setDBData } from "./global.js";
 const setContactsData = () => {
     const name = document.querySelector('.fullName').value 
     const email = document.querySelector('.email').value 
-    const adress = document.querySelector('.adress').value
+    const address = document.querySelector('.adress').value
     const phone = document.querySelector('.phone').value
     const note = documetn.querySelector('.note').value
+
+    if (!name && !email && !address && !phone && !note ) return
+
+    const userContactData = {
+        name,
+        email,
+        address,
+        phone,
+        note
+    }
 }
