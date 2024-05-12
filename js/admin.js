37d6f5a711b63d8336c135db75da1c4dfac1009e
Elvin <elvinnk@code.edu.az>
2024-05-12 21:12:58 +0400

    Added "Contact Us" information from firebase at the bottom of admin.html

--- js/admin.js
+++ js/admin.js
@@ -1,80 +1,83 @@
-import { getDBData, pushDBData, setDBData } from './global.js'
+import { getDBData, pushDBData, setDBData } from "./global.js";
 
-const search = document.querySelector('#searchBook')
-const searchResultList = document.querySelector('.search-result__list')
+const search = document.querySelector("#searchBook");
+const searchResultList = document.querySelector(".search-result__list");
 const bookInputs = {
-	name: document.getElementById('book-name'),
-	author: document.getElementById('author-name'),
-	imageUrl: document.getElementById('image-url'),
-	description: document.getElementById('description'),
-	bookType: document.getElementById('book-type'),
-}
+  name: document.getElementById("book-name"),
+  author: document.getElementById("author-name"),
+  imageUrl: document.getElementById("image-url"),
+  description: document.getElementById("description"),
+  bookType: document.getElementById("book-type"),
+};
 
-searchResultList.addEventListener('click', handleSearchResultClick)
+searchResultList.addEventListener("click", handleSearchResultClick);
 
 function handleSearchResultClick(event) {
-	const clickedListItem = event.target.closest('.search-result__list-item')
-	if (!clickedListItem) return
+  const clickedListItem = event.target.closest(".search-result__list-item");
+  if (!clickedListItem) return;
 
-	const { title, author, imageUrl, description, bookType } = clickedListItem.dataset
+  const { title, author, imageUrl, description, bookType } =
+    clickedListItem.dataset;
 
-	bookInputs.name.value = title || 'Unknown Title'
-	bookInputs.author.value = author || 'Unknown Author'
-	bookInputs.imageUrl.value = imageUrl || 'https://via.placeholder.com/150'
-	bookInputs.description.value = description || 'No description available'
-	bookInputs.bookType.value = bookType || 'Unknown Category'
+  bookInputs.name.value = title || "Unknown Title";
+  bookInputs.author.value = author || "Unknown Author";
+  bookInputs.imageUrl.value = imageUrl || "https://via.placeholder.com/150";
+  bookInputs.description.value = description || "No description available";
+  bookInputs.bookType.value = bookType || "Unknown Category";
 }
 
-const handleSearch = e => {
-	const searchTerm = e.target.value.trim()
+const handleSearch = (e) => {
+  const searchTerm = e.target.value.trim();
 
-	if (searchTerm === '') {
-		clearResults()
-		searchResultList.parentElement.classList.remove('search-result_show')
-		return
-	}
-	searchResultList.parentElement.classList.add('search-result_show')
-	fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`)
-		.then(response => response.json())
-		.then(data => {
-			const books = data.items.map(item => {
-				if (
-					item.volumeInfo.title &&
-					item.volumeInfo.authors &&
-					item.volumeInfo.authors.length > 0 &&
-					item.volumeInfo.imageLinks &&
-					item.volumeInfo.imageLinks.thumbnail &&
-					item.volumeInfo.description &&
-					item.volumeInfo.categories &&
-					item.volumeInfo.categories.length > 0
-				) {
-					const title = item.volumeInfo.title
-					const author = item.volumeInfo.authors[0]
-					const image = item.volumeInfo.imageLinks.thumbnail
-					const description = item.volumeInfo.description
-					const bookType = item.volumeInfo.categories[0]
-					return { title, author, image, description, bookType }
-				}
-			})
+  if (searchTerm === "") {
+    clearResults();
+    searchResultList.parentElement.classList.remove("search-result_show");
+    return;
+  }
+  searchResultList.parentElement.classList.add("search-result_show");
+  fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const books = data.items.map((item) => {
+        if (
+          item.volumeInfo.title &&
+          item.volumeInfo.authors &&
+          item.volumeInfo.authors.length > 0 &&
+          item.volumeInfo.imageLinks &&
+          item.volumeInfo.imageLinks.thumbnail &&
+          item.volumeInfo.description &&
+          item.volumeInfo.categories &&
+          item.volumeInfo.categories.length > 0
+        ) {
+          const title = item.volumeInfo.title;
+          const author = item.volumeInfo.authors[0];
+          const image = item.volumeInfo.imageLinks.thumbnail;
+          const description = item.volumeInfo.description;
+          const bookType = item.volumeInfo.categories[0];
+          return { title, author, image, description, bookType };
+        }
+      });
 
-			const filteredBooks = books.filter(book => book)
+      const filteredBooks = books.filter((book) => book);
 
-			renderResults(filteredBooks)
-		})
-		.catch(error => {
-			console.error('Error fetching books:', error)
-		})
-}
+      renderResults(filteredBooks);
+    })
+    .catch((error) => {
+      console.error("Error fetching books:", error);
+    });
+};
 
-const renderResults = results => {
-	clearResults()
+const renderResults = (results) => {
+  clearResults();
 
-	results.forEach(result => {
-		const { title, author } = result
-		const shortenedTitle = title.length > 10 ? `${title.slice(0, 10)}...` : title
-		const shortenedAuthor = author.length > 10 ? `${author.slice(0, 10)}...` : author
+  results.forEach((result) => {
+    const { title, author } = result;
+    const shortenedTitle =
+      title.length > 10 ? `${title.slice(0, 10)}...` : title;
+    const shortenedAuthor =
+      author.length > 10 ? `${author.slice(0, 10)}...` : author;
 
-		searchResultList.innerHTML += `
+    searchResultList.innerHTML += `
             <li class="search-result__list-item" 
                 data-title="${title}" 
                 data-author="${author}" 
@@ -98,134 +101,162 @@ const renderResults = results => {
                 <p class="search-result__author">${shortenedAuthor}</p>
                 <h3 class="search-result__title">${shortenedTitle}</h3>
             </li>
-        `
-	})
-}
+        `;
+  });
+};
 
-const clearResults = () => (searchResultList.innerHTML = '')
+const clearResults = () => (searchResultList.innerHTML = "");
 
-search.addEventListener('input', handleSearch)
+search.addEventListener("input", handleSearch);
 
 const pushBookInDB = async () => {
-	const title = bookInputs.name.value.trim()
-	const author = bookInputs.author.value.trim()
-	const imageUrl = bookInputs.imageUrl.value.trim()
-	const description = bookInputs.description.value.trim()
-	const bookType = bookInputs.bookType.value.trim()
+  const title = bookInputs.name.value.trim();
+  const author = bookInputs.author.value.trim();
+  const imageUrl = bookInputs.imageUrl.value.trim();
+  const description = bookInputs.description.value.trim();
+  const bookType = bookInputs.bookType.value.trim();
 
-	// Проверка на пустые значения инпутов
-	if (!title || !author || !imageUrl || !description || !bookType) {
-		alert('All fields must be filled out')
-		return
-	}
-	const bookInfo = {
-		title: bookInputs.name.value,
-		description: bookInputs.description.value,
-		category: bookInputs.bookType.value,
-		image: bookInputs.imageUrl.value,
-		author: bookInputs.author.value,
-	}
+  // Проверка на пустые значения инпутов
+  if (!title || !author || !imageUrl || !description || !bookType) {
+    alert("All fields must be filled out");
+    return;
+  }
+  const bookInfo = {
+    title: bookInputs.name.value,
+    description: bookInputs.description.value,
+    category: bookInputs.bookType.value,
+    image: bookInputs.imageUrl.value,
+    author: bookInputs.author.value,
+  };
 
-	try {
-		await pushDBData('/books', bookInfo)
-		console.log('Book data successfully written to the database')
-	} catch (error) {
-		console.error('Error writing book data to the database:', error)
-	}
+  try {
+    await pushDBData("/books", bookInfo);
+    console.log("Book data successfully written to the database");
+  } catch (error) {
+    console.error("Error writing book data to the database:", error);
+  }
 
-	bookInputs.name.value = ''
-	bookInputs.author.value = ''
-	bookInputs.imageUrl.value = ''
-	bookInputs.description.value = ''
-	bookInputs.bookType.value = ''
-}
+  bookInputs.name.value = "";
+  bookInputs.author.value = "";
+  bookInputs.imageUrl.value = "";
+  bookInputs.description.value = "";
+  bookInputs.bookType.value = "";
+};
 
-document.querySelector('.addBookddDatabase').addEventListener('click', async e => {
-	e.preventDefault()
-	await pushBookInDB()
-})
+document
+  .querySelector(".addBookddDatabase")
+  .addEventListener("click", async (e) => {
+    e.preventDefault();
+    await pushBookInDB();
+  });
 
 // About Store
 const setAboutStoreInfoToDB = async () => {
-	const aboutStoreTitleInput = document.getElementById('aboutStore')
-	const aboutStoreImageInput = document.getElementById('bookImageUrl')
-	const aboutStoreDescriptionInput = document.getElementById('descriptionFor')
+  const aboutStoreTitleInput = document.getElementById("aboutStore");
+  const aboutStoreImageInput = document.getElementById("bookImageUrl");
+  const aboutStoreDescriptionInput = document.getElementById("descriptionFor");
 
-	const aboutStoreData = {
-		title: aboutStoreTitleInput.value.trim(),
-		image: aboutStoreImageInput.value.trim(),
-		description: aboutStoreDescriptionInput.value.trim(),
-	}
-	console.log(aboutStoreData)
-	const { title, image, description } = aboutStoreData
+  const aboutStoreData = {
+    title: aboutStoreTitleInput.value.trim(),
+    image: aboutStoreImageInput.value.trim(),
+    description: aboutStoreDescriptionInput.value.trim(),
+  };
+  console.log(aboutStoreData);
+  const { title, image, description } = aboutStoreData;
 
-	if (!title && !image && !description) return alert('All fields are required!')
+  if (!title && !image && !description)
+    return alert("All fields are required!");
 
-	await setDBData('/aboutStore', aboutStoreData)
+  await setDBData("/aboutStore", aboutStoreData);
 
-	aboutStoreTitleInput.value = ''
-	aboutStoreImageInput.value = ''
-	aboutStoreDescriptionInput.value = ''
-}
+  aboutStoreTitleInput.value = "";
+  aboutStoreImageInput.value = "";
+  aboutStoreDescriptionInput.value = "";
+};
 
-document.getElementById('addInfoButton').addEventListener('click', setAboutStoreInfoToDB)
+document
+  .getElementById("addInfoButton")
+  .addEventListener("click", setAboutStoreInfoToDB);
 
 // ------------------Join Us----------------------
 
-await getDBData('/users').then(data => {
-	const users = Object.values(data)
-	const userNumber = document.querySelector('#JoinUsLi')
-	const userFullName = document.querySelector('#join-us-fullname')
-	const userEmail = document.querySelector('#join-us-email')
+await getDBData("/users").then((data) => {
+  const users = Object.values(data);
+  const userNumber = document.querySelector("#JoinUsLi");
+  const userFullName = document.querySelector("#join-us-fullname");
+  const userEmail = document.querySelector("#join-us-email");
 
-	users.forEach((user, index) => {
-		userNumber.innerHTML += `
+  users.forEach((user, index) => {
+    userNumber.innerHTML += `
 		<li class="join-us__list-item">${index + 1}</li>
-		`
-		userFullName.innerHTML += `
+		`;
+    userFullName.innerHTML += `
 		<li class="join-us__list-item"><span>${user.name}</span></li>
-		`
-		userEmail.innerHTML += `
+		`;
+    userEmail.innerHTML += `
 		<li class="join-us__list-item"><span>${user.email}</span></li>
-		`
-	})
-})
+		`;
+  });
+});
 
 // -------------------Books--------------------------
-await getDBData('/books').then(data => {
-	const books = Object.values(data)
+await getDBData("/books").then((data) => {
+  const books = Object.values(data);
 
-	const booksNumbers = document.querySelector('#booksNumber')
-	const booksTitles = document.querySelector('#booksTitle')
-	const booksDesc = document.querySelector('#booksDesc')
-	const booksType = document.querySelector('#booksType')
-	const booksAuthor = document.querySelector('#booksAuthor')
+  const booksNumbers = document.querySelector("#booksNumber");
+  const booksTitles = document.querySelector("#booksTitle");
+  const booksDesc = document.querySelector("#booksDesc");
+  const booksType = document.querySelector("#booksType");
+  const booksAuthor = document.querySelector("#booksAuthor");
 
-	books.forEach((book, index) => {
-		let title = book?.title?.length > 20 ? book.title.slice(0, 20) + '...' : book.title
-		let description = book?.description?.length > 20 ? book.description.slice(0, 20) + '...' : book.description
-		booksNumbers.innerHTML += `
-            <li class="join-us__list-item join-us__list-item_flex">${index + 1}</li>
-        `
-		booksTitles.innerHTML += `
+  books.forEach((book, index) => {
+    let title =
+      book?.title?.length > 20 ? book.title.slice(0, 20) + "..." : book.title;
+    let description =
+      book?.description?.length > 20
+        ? book.description.slice(0, 20) + "..."
+        : book.description;
+    booksNumbers.innerHTML += `
+            <li class="join-us__list-item join-us__list-item_flex">${
+              index + 1
+            }</li>
+        `;
+    booksTitles.innerHTML += `
             <li class="join-us__list-item join-us__list-item_flex">
                 <img src="${book?.image}" alt="book images">
                 <span>${title}</span>
             </li>
-        `
-		booksDesc.innerHTML += `
+        `;
+    booksDesc.innerHTML += `
             <li class="join-us__list-item join-us__list-item_flex"><span>${description}</span></li>
-        `
-		booksType.innerHTML += `
+        `;
+    booksType.innerHTML += `
             <li class="join-us__list-item join-us__list-item_flex"><span>${book.category}</span></li>
-        `
-		booksAuthor.innerHTML += `
+        `;
+    booksAuthor.innerHTML += `
             <li class="join-us__list-item join-us__list-item_flex"><span>${book.author}</span></li>
-        `
-	})
-})
+        `;
+  });
+});
 // ---------------------------Contact-----------------------------
 
-await getDBData('/contacs').then(contacts => {
-	// console.log(contacts)
-})
+await getDBData("/contacts").then((data) => {
+  const contacts = Object.values(data);
+  console.log(contacts);
+
+  const contactsNumber = document.querySelector("#contactsNumber");
+  const contactsFullName = document.querySelector("#contactsFullName");
+  const contactsAddress = document.querySelector("#contactsAddress");
+  const contactsEmail = document.querySelector("#contactsEmail");
+  const contactsPhoneNumber = document.querySelector("#contactsPhoneNumber");
+
+  contacts.forEach((contact, index) => {
+    contactsNumber.innerHTML += `<li class="join-us__list-item">${
+      index + 1
+    }</li>`;
+    contactsFullName.innerHTML += `<li class="join-us__list-item join-us__list-item_flex"><span>${contact.name}</span></li>`;
+    contactsAddress.innerHTML += `<li class="join-us__list-item join-us__list-item_flex"><span>${contact.address}</span></li>`;
+    contactsEmail.innerHTML += `<li class="join-us__list-item join-us__list-item_flex"><span>${contact.email}</span></li>`;
+    contactsPhoneNumber.innerHTML += `<li class="join-us__list-item join-us__list-item_flex"><span>${contact.phone}</span></li>`;
+  });
+});
